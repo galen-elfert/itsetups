f3e915638ee83a077e12fc0f5b587e329220e1a4
Galen Elfert <gelfert@gelfert.localdomain>
2016-04-11 17:19:18 -0700

    Working TTX parser. Added test imput data

--- client/main.js
+++ client/main.js
@@ -64,14 +64,14 @@ parseTTXGood = function(contents) {
     var lines = contents.split('\n').map(line => {return line.trim();});
     var exported = new Date(lines.shift());
     console.log("exported:", exported);
-    // Remove the rest of the header
-    lines = lines.slice(4);
-    // And footer
-    lines.pop();
-    lines.pop();
+    // Remove the rest of the header, and footer
+    lines = lines.slice(3, -3);
+    if(Date.parse(lines[0].split('-')[0])) { lines.shift(); }
+
+    // Group lines into events, by on vertical separation
     var eventBlobs = [[]];
     lines.forEach(function (line) {
-        if(line != '') {
+        if(line !== '') {
             if (count >= 8) {
                 eventBlobs.push([]);
             }
@@ -81,6 +81,15 @@ parseTTXGood = function(contents) {
             count++;
         }
     });
+
+    // Gather orphaned lines (certain line types seem to get split accross pages)
+    for (var i = 0; i < eventBlobs.length; i++) {
+        if(eventBlobs[i].length < 3) {
+            eventBlobs[i-1].push(...eventBlobs[i]);
+            eventBlobs.splice(i,1);
+        }
+    }
+
     var events = [];
     var building, date;
     eventBlobs.forEach(function (blob) {
@@ -94,7 +103,6 @@ parseTTXGood = function(contents) {
             console.log('Date:', date);
             blob = blob.slice(2);
         }
-        console.log(blob[0])
         var [start, end] = blob.shift().split('\t').slice(0,2);
         let thisEvent = {
             exported: exported,
@@ -107,10 +115,10 @@ parseTTXGood = function(contents) {
         thisEvent.timeEnd.setHours(...end.split(':'));
         var details = blob.shift().split('\t').map(word => {return s.trim(word, '"');});
         if (details[0]) {
-            thisEvent.onsite = details[0];
+            thisEvent.onsite = details[0].trim().slice(8);
         }
         thisEvent.number = details[1].slice(1,6);
-        thisEvent.name = details[1].slice(6);
+        thisEvent.name = details[1].slice(7);
         if (details[2]) {
             thisEvent.contact = details[2];
         }
@@ -126,7 +134,6 @@ parseTTXGood = function(contents) {
                 thisEvent.resources[thisEvent.resources.length - 1].notes = words[0];
             }
         });
-        console.log('This event:', thisEvent);
         events.push(thisEvent);
     });
     console.log(events);
